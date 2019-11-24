b23b153d804a90202797c6b93063f80b204d0041
yanwk <yanwenkun@foxmail.com>
2019-11-24 08:12:09 +0800

    完成日志配置相关编写

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        LogLevels logLvs = new LogLevels();
+    }
+
+    static class LogLevels {
+        private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogLevels.class);
+
+        LogLevels() {
+            log.error("显示ERROR级日志");
+            log.warn("显示WARN级日志");
+            log.info("显示INFO级日志");
+            log.debug("显示DEBUG级日志");
+            log.trace("显示TRACE级日志");
+        }
     }
 
 }
